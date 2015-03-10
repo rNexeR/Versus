4c98e16f2a171f2b5ee51ecb137c8b200ab556a3
rnexer <rnexer@gmail.com>
2015-03-09 20:22:51 -0600

    Tecla Presionada

--- main.cpp
+++ main.cpp
@@ -5,11 +5,19 @@
 #include "allegro5/allegro_native_dialog.h"
 using namespace std;
 
+const float FPS = 60;
+
 ALLEGRO_DISPLAY *display = NULL;
 ALLEGRO_EVENT_QUEUE *event_queue = NULL;
 ALLEGRO_BITMAP  *splash   = NULL;
 
-int initAllegro(){
+//EVENTOS Y TIMERS
+ALLEGRO_EVENT ev;
+ALLEGRO_TIMEOUT timeout;
+ALLEGRO_TIMER *timer = NULL;
+
+int initAllegro()
+{
     if(!al_init())
     {
         cout<<"failed to initialize allegro!\n"<<endl;
@@ -37,11 +45,25 @@ int initAllegro(){
         return -1;
     }
 
+    timer = al_create_timer(1.0 / FPS);
+    if(!timer) {
+        cout<<"failed to create timer!"<<endl;
+    }
+
+    if(!al_install_keyboard()) {
+      cout<<"failed to initialize the keyboard!"<<endl;
+    }
+
     al_register_event_source(event_queue, al_get_display_event_source(display));
+    al_register_event_source(event_queue, al_get_timer_event_source(timer));
+    al_register_event_source(event_queue, al_get_keyboard_event_source());
+
+    al_init_timeout(&timeout, 0.06);
     return 0;
 }
 
-int initSplash(){
+int initSplash()
+{
     splash = al_load_bitmap("Versus.png");
 
     if(!splash)
@@ -53,18 +75,30 @@ int initSplash(){
     return 0;
 }
 
-void mainMenu(){
-    while(true){
+void mainMenu()
+{
+    while(true)
+    {
 
     }
 }
 
-bool teclaPresionada(){
-    //Codigo
-    return false;
+void teclaPresionada(int keycode, bool *variable)
+{
+    if(ev.type == ALLEGRO_EVENT_KEY_DOWN)
+    {
+        if(ev.keyboard.keycode==keycode)
+            *variable = true;
+    }
+     if(ev.type == ALLEGRO_EVENT_KEY_UP)
+    {
+        if(ev.keyboard.keycode==keycode)
+            *variable = false;
+    }
 }
 
-void showSplash(){
+void showSplash()
+{
     al_clear_to_color(al_map_rgb(0,0,0));
     al_draw_bitmap(splash,300,150,0);
     al_flip_display();
@@ -76,18 +110,21 @@ int main(int argc, char **argv)
     if (initAllegro()<0 || initSplash()<0)
         return -1;
     showSplash();
+
+    bool UP = false;
+
     while(true)
     {
-        ALLEGRO_EVENT ev;
-        ALLEGRO_TIMEOUT timeout;
-        al_init_timeout(&timeout, 0.06);
-
+        //cout<<"FOREVER"<<endl;
         bool get_event = al_wait_for_event_until(event_queue, &ev, &timeout);
-
         if(get_event && ev.type == ALLEGRO_EVENT_DISPLAY_CLOSE)
         {
             break;
         }
+        teclaPresionada(ALLEGRO_KEY_UP, &UP);
+        if (UP)
+            cout<<"Presionada"<<endl;
+
         al_clear_to_color(al_map_rgb(0,0,0));
 
         al_flip_display();
