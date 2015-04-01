6f1c87eb3b2e4ad40adba3dd1b311167dbe7ecd5
Raimmaster <rai.interiano@gmail.com>
2015-04-01 14:03:52 -0600

    Agrega la super-clase Entidad de la cual heredarán todos los objetos

---
+++ Entidad.cpp
@@ -0,0 +1,22 @@
+#include "Entidad.h"
+
+Entidad::Entidad()
+{
+    //ctor
+}
+
+Entidad::~Entidad()
+{
+    //dtor
+}
+
+//Detecta si el objeto está colisionando o no
+bool PersonajesAnimados::colision(Box* pCaja){
+    if(pCaja->x + pCaja->width < detalles->x
+        || pCaja->x > detalles->x + pCaja->width
+        || pCaja->y + pCaja->height < detalles->y
+        || pCaja->y > detalles->y + pCaja->height)
+        return false;
+    else
+        return true;
+}
---
+++ Entidad.h
@@ -0,0 +1,33 @@
+#ifndef ENTIDAD_H
+#define ENTIDAD_H
+//Librerías y archivos necesarios
+#include "Box.h"
+#include <allegro5/allegro.h>
+#include "allegro5/allegro_image.h"
+#include "allegro5/allegro_native_dialog.h"
+#include <allegro5/allegro_audio.h>
+#include <allegro5/allegro_acodec.h>
+#include "ObjetosAnimados.h"
+#include <iostream>
+#include <map>
+#include <vector>
+#include <list>
+#include <fstream>
+
+class Entidad
+{
+    public:
+        //Atributos
+        bool colisionado;
+        Box *detalles = new Box(0,0,0,0); //donde estará y de donde se comprueban las colisiones
+        int velocity = 1; //cuanto se mueve
+        virtual void act(ALLEGRO_EVENT* ev) = 0;
+        virtual void draw() = 0;//dibujo
+        bool colision(Box* pCaja);//detectar colisiones con otros objetos
+        Entidad();
+        virtual ~Entidad();
+    protected:
+    private:
+};
+
+#endif // ENTIDAD_H
