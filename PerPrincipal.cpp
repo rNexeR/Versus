3608defacb38bbe3601b0aaf5f477f2e3401f6eb
rnexer <rnexer@gmail.com>
2015-04-02 15:24:57 -0600

    Reservar samples

--- PerPrincipal.cpp
+++ PerPrincipal.cpp
@@ -27,7 +27,7 @@ PerPrincipal::PerPrincipal(ALLEGRO_EVENT_QUEUE *event_queue, list<PersonajesAnim
         return;
     }
 
-    if (!al_reserve_samples(2)){
+    if (!al_reserve_samples(15)){
         fprintf(stderr, "failed to reserve samples!\n");
         return;
     }
