5a8cd0aac91ba99e681616252b42bb4b9a4ae458
Raimmaster <rai.interiano@gmail.com>
2015-04-05 00:14:48 -0600

    GPL3 license notice in other files

--- Obstaculo.h
+++ Obstaculo.h
@@ -1,3 +1,22 @@
+/**
+	Versus is a PC game programmed in C++ using the Allegro 5.0.11 library.
+	Copyright (C) 2015  Nexer Rodríguez and Ricardo A. Interiano (Raim)
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+**/
+
 #ifndef OBSTACULO_H
 #define OBSTACULO_H
 
